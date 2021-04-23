59cffc29612b33fc73ebc586c1ca7e221d4f69c2
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-04-23 19:05:58 -0300

    Create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header>
+            <div className="container">
+                <h1 className="title"></h1>
+            </div>
+        </header>
+    )
+}
